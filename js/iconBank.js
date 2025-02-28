3ad6162a7916c76744f0ab7f9be3e42f1a427014
mitchmarns <sakurapizzaa@gmail.com>
2025-02-27 20:26:12 -0800

    Add files via upload

---
+++ js/iconBank.js
@@ -0,0 +1,11 @@
+export const iconBank = {
+  cat: { src: "../icons/cat.png", alt: "Cat", tooltip: "This character has a pet cat" },
+  college: { src: "../icons/college.png", alt: "College", tooltip: "This character has a college education" },
+  dance: { src: "../icons/dance.png", alt: "Dance", tooltip: "This character likes to dance" },
+  dog: { src: "../icons/dog.png", alt: "Dog", tooltip: "This character has a dog" },
+  goalie: { src: "../icons/goalie.png", alt: "Goalie", tooltip: "This character is a goalie" },
+  gym: { src: "../icons/gym.png", alt: "Gym", tooltip: "This character is a gym rat" },
+  hockey: { src: "../icons/hockey.png", alt: "Hockey", tooltip: "This character plays hockey" },
+  weed: { src: "../icons/weed.png", alt: "Dog", tooltip: "This character is 420 friendly" },
+  // Add more icons as needed
+};
